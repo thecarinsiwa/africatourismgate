0c9ab0a59f67c6737e8f07dec9d0b5c3fd8a5a04
thecarinsiwa <thecarinsiwa@gmail.com>
2026-06-12 17:42:51 +0300

    Créer controller + module et enregistrer dans PublicModule

--- apps/api/src/modules/public/public.module.ts
+++ apps/api/src/modules/public/public.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PublicAccommodationsModule } from './accommodations/public-accommodations.module';
 import { PublicFlightsModule } from './flights/public-flights.module';
+import { PublicVehiclesModule } from './vehicles/public-vehicles.module';
 
 @Module({
-  imports: [PublicAccommodationsModule, PublicFlightsModule],
+  imports: [
+    PublicAccommodationsModule,
+    PublicFlightsModule,
+    PublicVehiclesModule,
+  ],
 })
 export class PublicModule {}
