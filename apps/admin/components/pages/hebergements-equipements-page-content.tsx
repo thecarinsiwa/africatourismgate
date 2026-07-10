85413da57bee8f2ea0f95a14318333d4e9176dc2
thecarinsiwa <thecarinsiwa@gmail.com>
2026-07-10 12:14:16 +0300

    Add statistic  /hebergements/equipements

--- apps/admin/components/pages/hebergements-equipements-page-content.tsx
+++ apps/admin/components/pages/hebergements-equipements-page-content.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { AmenitiesList } from '../amenities/amenities-list';
+import { PropertiesStatCards } from '../properties/properties-stat-cards';
 import { AdminIntroPage } from './admin-intro-page';
 
 export function EquipementsPageContent() {
   return (
     <AdminIntroPage routePath="hebergements/equipements">
+      <PropertiesStatCards className="mb-6" />
       <AmenitiesList />
     </AdminIntroPage>
   );
