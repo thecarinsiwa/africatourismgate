71cbf9d7d5ffe4d4d147be57181dac1b1037e9d5
thecarinsiwa <thecarinsiwa@gmail.com>
2026-08-20 20:13:33 +0300

    Fix employee account stat i28n on pages

--- apps/admin/components/pages/utilisateurs-departements-page-content.tsx
+++ apps/admin/components/pages/utilisateurs-departements-page-content.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { DepartmentsList } from '../departments/departments-list';
+import { DepartmentsStatCards } from '../departments/departments-stat-cards';
 import { AdminIntroPage } from './admin-intro-page';
 
 export function DepartementsPageContent() {
   return (
     <AdminIntroPage routePath="utilisateurs/departements">
+      <DepartmentsStatCards className="mb-6" />
       <DepartmentsList />
     </AdminIntroPage>
   );
