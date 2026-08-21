31d95a0d11ea96cee04e530d88c77f07254b6769
thecarinsiwa <thecarinsiwa@gmail.com>
2026-08-21 20:20:37 +0300

    Add bouton Retour aux départs

--- apps/admin/components/pages/produits-croisieres-lignes-page-content.tsx
+++ apps/admin/components/pages/produits-croisieres-lignes-page-content.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 import { CruisesStatCards } from '../cruises/cruises-stat-cards';
 import { CruiseLinesList } from '../cruises/cruise-lines-list';
 import { AdminIntroPage } from './admin-intro-page';
 
 export function LignesCroisierePageContent() {
   return (
-    <AdminIntroPage routePath="produits/croisieres/lignes">
+    <AdminIntroPage
+      routePath="produits/croisieres/lignes"
+      backHref="/produits/croisieres"
+      backLabelKey="backLabel"
+    >
       <CruisesStatCards className="mb-6" />
       <CruiseLinesList />
     </AdminIntroPage>
