48a166c6bc003a55e480d1d928439cc2ba6d6694
thecarinsiwa <thecarinsiwa@gmail.com>
2026-09-22 01:58:06 +0300

    Page hub /aide + AidePageContent

---
+++ apps/admin/components/pages/aide-page-content.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import { useSetAdminPageMeta } from '../admin-page-meta-context';
+import { AdminHelpCategoryGrid } from '../admin-help/admin-help-category-grid';
+import { AdminHelpPopularList } from '../admin-help/admin-help-popular-list';
+import { AdminHelpSearch } from '../admin-help/admin-help-search';
+import { AdminListPageHeader } from './admin-list-page-header';
+
+export function AidePageContent() {
+  const t = useTranslations('pages.aide');
+  useSetAdminPageMeta({ title: t('title') });
+
+  return (
+    <div className="min-w-0">
+      <AdminListPageHeader routePath="aide" />
+
+      <div className="mx-auto w-full max-w-4xl space-y-12">
+        <div className="max-w-2xl">
+          <AdminHelpSearch />
+        </div>
+
+        <AdminHelpCategoryGrid />
+        <AdminHelpPopularList />
+      </div>
+    </div>
+  );
+}
